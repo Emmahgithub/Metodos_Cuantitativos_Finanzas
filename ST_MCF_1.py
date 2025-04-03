764bc87935c959ad0773d57fdbd21be414e348c6
Emmanuel Vargas Bautista <143526287+Emmahgithub@users.noreply.github.com>
2025-04-02 22:53:19 -0600

    Update ST_MCF_1.py

--- ST_MCF_1.py
+++ ST_MCF_1.py
@@ -193,8 +193,8 @@ if stock_seleccionado:
     print(ESH_rolling_df_99)
 
     fig, ax = plt.subplots(figsize=(12, 6))
-    ax.plot(df_rendimientos.index, df_rendimientos[stock_seleccionado] , label='Daily Returns (%)', color='blue', alpha=0.5)
-    ax.plot(ESN_rolling_df_95.index, ESN_rolling_df_95['0.95% ESN Rolling'] *100, label='0.95% ESN Rolling', color='red')
+    ax.plot(df_rendimientos.index, df_rendimientos[stock_seleccionado] * 100, label='Daily Returns (%)', color='blue', alpha=0.5)
+    ax.plot(ESN_rolling_df_95.index, ESN_rolling_df_95['0.95% ESN Rolling'] , label='0.95% ESN Rolling', color='red')
     ax.set_title('Retornos diaros, 0.95% VaR Rolling y 0.95% ESN Rolling')
     ax.set_xlabel('Date')
     ax.set_ylabel('Values (%)')
